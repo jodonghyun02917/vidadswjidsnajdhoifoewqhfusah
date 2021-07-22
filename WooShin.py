7b1a094ccc4e91d684c78637d10d5a06804fb329
jodonghyun02917 <58250960+jodonghyun02917@users.noreply.github.com>
2021-07-22 17:43:14 +0900

    Add files via upload

---
+++ WooShin.py
@@ -0,0 +1,39 @@
+import discord
+import asyncio
+import datetime as dt
+token = 'ODY1NjAzNjI4Njk3NzgwMjQ0.YPGaMw.hHef4P_BbnDZFKtIKo4o6-jARGE'
+client = discord.Client()
+
+
+f = open("급식.txt", 'r')
+y = f.read()
+f.close()
+
+@client.event
+async def on_ready():
+    print("bot ready!")
+    print(client.user)
+    print("=========================")
+    
+
+
+i = 1
+async def a():
+    await client.wait_until_ready()
+    channel = client.get_channel(866887465172336650)
+    while not client.is_closed():
+        global i, x
+        f = open(f"7_{i}.txt", 'r')
+        x = f.read()
+        f.close()
+        await channel.send("```"+x+"```")
+        i += 1
+        await asyncio.sleep(10)
+@client.event
+async def on_message(message): #사용자가 메시지를 입력했을 때
+    if message.content == "!급식":
+        await message.channel.send("```"+x+"```")
+
+        
+client.loop.create_task(a())
+client.run(token)
